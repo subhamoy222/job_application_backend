80465807886ee482f09d71a36c145f482b9e02e6
Subhamoy Sasmal <2023aspire117@gmail.com>
2025-05-17 17:54:43 +0530

    Initial commit for job application frontend

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ import fileUpload from "express-fileupload";
 const app = express();
 config({ path: "./config/config.env" });
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    method: ["GET", "POST", "DELETE", "PUT"],
-    credentials: true,
-  })
-);
+app.options("*", cors({
+  origin: [process.env.FRONTEND_URL],
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true,
+}));
+
 
 app.use(cookieParser());
 app.use(express.json());
