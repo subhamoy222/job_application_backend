cadb50ffa4ff5b1c67bee40f0e3c6393912d7532
Subhamoy Sasmal <2023aspire117@gmail.com>
2025-06-17 23:46:07 +0530

    some chnages in cors allownace

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -15,18 +15,35 @@ import { catchAsyncErrors } from './catchAsyncError.js';
 import ErrorHandler from "./error.js";
 import jwt from "jsonwebtoken";
 
+// export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+//   const { token } = req.cookies;
+//   if (!token) {
+//     return next(new ErrorHandler("User Not Authorized", 401));
+//   }
+//   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+//   req.user = await User.findById(decoded.id);
+
+//   next();
+// });
+
+
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
+  console.log("Token from cookies:", token); // Debug line
+  
   if (!token) {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
+  
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  console.log("Decoded token:", decoded); // Debug line
 
   req.user = await User.findById(decoded.id);
+  console.log("User found:", req.user?.email); // Debug line
 
   next();
 });
-
 
 
 
